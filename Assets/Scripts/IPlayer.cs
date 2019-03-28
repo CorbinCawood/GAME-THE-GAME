8af4ec5a4a0086d465527d6442b05aa8cebfa32a
Preston Smith <psmith79@vols.utk.edu>
2019-03-28 16:35:14 -0400

    Added some base interfaces as well as some skeleton for future code. - Preston

---
+++ Assets/Scripts/IPlayer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public interface IPlayer
+{
+    bool MainAttack();
+    bool SecondaryAttack();
+    bool TertiaryAttack();
+    
+}
+
+public interface INPCPlayer
+{
+    
+}
+public class Quest
+{
+    
+}
+
+
+public class SkillNodeAttribute
+{
+}
+
+public class SkillNode
+{
+}
+
+public struct NPCRelation
+{
+    public bool HaveMet { get; set;}
+    public Dictionary<int, Quest> QuestList;
+
+}
+public class Player : IPlayer
+{
+
+    public string Name { get; set; }
+    public Dictionary<string,List<string>> Characteristics { get; set; }
+    public Dictionary<string, int> Inventory { get; set; }
+    public Dictionary<string, int> Stats { get; set;}
+    public Dictionary<string, int> Abilities { get; set; }
+    public Dictionary<SkillNode, List<SkillNodeAttribute>> SkillTree;
+    public Dictionary<string, NPCRelation> NPCRelations;
+    
+    public bool MainAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool SecondaryAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool TertiaryAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
