19283d387994dca41f66f1b714d3e826927472fa
zrandall <42679008+zrandall@users.noreply.github.com>
2019-04-27 20:41:02 -0400

    Added Comments

--- follow.cs
+++ follow.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//This is where the follow behavior is defined for the enemies when they are within range of the hero.
+
 public class follow : MonoBehaviour
 {
     public float speed;
@@ -10,12 +12,14 @@ public class follow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Find the player
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Update the position each frame
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
